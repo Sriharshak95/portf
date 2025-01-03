88cc969c05276cf78b23b6aa1ae55b352911ae3b
Sriharsha K <harsha.daggron@gmail.com>
2025-01-03 16:48:14 +0530

    basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    reactStrictMode: true
+    reactStrictMode: true,
+    basePath: '/portf' || ''
 };
 
 export default nextConfig;
