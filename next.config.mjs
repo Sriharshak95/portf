6e6f9297ed5d423ae2a5055d81c416ed956a2fd1
Sriharsha K <harsha.daggron@gmail.com>
2025-01-03 17:11:30 +0530

    basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
 const nextConfig = {
     output: "export",
     reactStrictMode: true,
-    basePath: '/portf' || '',
-    images: {
-        unoptimized: true
-    }
+    'process.env.BACKEND_URL': !debug ? 'https://sriharshak95.github.io/portf' : ''
 };
 
 export default nextConfig;
